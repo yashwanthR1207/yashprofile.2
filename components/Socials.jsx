cce94e87e1bdd41cefcab73d6dba53588d2b0d5d
yashwanthR1207 <yashwanth.1207.r@gmail.com>
2026-07-03 00:04:24 +0530

    m

---
+++ components/Socials.jsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+
+import {
+  RiLinkedinLine,
+  RiGithubLine,
+  RiMailLine,
+} from "react-icons/ri";
+
+export const socialData = [
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/yashwanth-r-7855a7395",
+    Icon: RiLinkedinLine,
+  },
+  {
+    name: "Github",
+    link: "https://github.com/yashwanthR1207",
+    Icon: RiGithubLine,
+  },
+  {
+    name: "Email",
+    link: "mailto:yashwanth.1207.r@gmail.com",
+    Icon: RiMailLine,
+  },
+];
+
+const Socials = () => {
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      {socialData.map((social, i) => (
+        <Link
+          key={i}
+          title={social.name}
+          href={social.link}
+          target="_blank"
+          rel="noreferrer noopener"
+          className={`${
+            social.name === "Github"
+              ? "bg-accent rounded-full p-[5px] hover:text-white"
+              : "hover:text-accent"
+          } transition-all duration-300`}
+        >
+          <social.Icon aria-hidden />
+          <span className="sr-only">{social.name}</span>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Socials;
