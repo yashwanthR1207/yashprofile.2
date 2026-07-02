ff23728448a50aa6595e4effa15a518fd602cc0a
yashwanthR1207 <yashwanth.1207.r@gmail.com>
2026-07-03 01:18:06 +0530

    Update browser tab title to Yashwanth

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -19,16 +19,16 @@ const Layout = ({ children }) => {
     >
       {/* metadata */}
       <Head>
-        <title>Ethan Smith | Portfolio</title>
+        <title>Yashwanth | Portfolio</title>
         <meta
           name="description"
-          content="Ethan Smith is a Full-stack web developer with 10+ years of experience."
+          content="Yashwanth is a passionate developer."
         />
         <meta
           name="keywords"
-          content="react, next, nextjs, html, css, javascript, js, modern-ui, modern-ux, portfolio, framer-motion, 3d-website, particle-effect"
+          content="react, next, nextjs, html, css, javascript, js, modern-ui, modern-ux, portfolio, framer-motion, 3d-website, particle-effect, iot, developer"
         />
-        <meta name="author" content="Sanidhya Kumar Verma" />
+        <meta name="author" content="Yashwanth" />
         <meta name="theme-color" content="#f13024" />
       </Head>
 
