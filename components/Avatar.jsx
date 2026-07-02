100ced2929e12ad72eae918cbd082d531af3440b
yashwanthR1207 <yashwanth.1207.r@gmail.com>
2026-07-03 02:25:46 +0530

    Add new avatar picture with background removed

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -4,11 +4,11 @@ const Avatar = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none pointer-events-none select-none relative w-full h-full justify-center items-end">
       <Image
-        src="/avatar.png"
+        src="/avatar_new.png"
         alt="avatar"
-        width={737}
-        height={678}
-        className="translate-z-0 w-auto h-full object-contain object-bottom relative z-10"
+        fill
+        sizes="(max-width: 1200px) 100vw, 50vw"
+        className="translate-z-0 object-contain object-bottom relative z-10 drop-shadow-2xl"
       />
     </div>
   );
