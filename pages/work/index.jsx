d34bb8c171b07ba84ff40d94ec7ef3be9f319367
yashwanthR1207 <yashwanth.1207.r@gmail.com>
2026-07-03 03:24:04 +0530

    Add popping cat image to the Work page

--- pages/work/index.jsx
+++ pages/work/index.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 import Bulb from "../../components/Bulb";
 import Circles from "../../components/Circles";
@@ -31,6 +32,25 @@ const Work = () => {
             >
               Innovating through practical hardware solutions and intelligent software design.
             </motion.p>
+            {/* Pop-up Image */}
+            <motion.div
+              initial={{ scale: 0, opacity: 0, y: 50 }}
+              animate={{ scale: 1, opacity: 1, y: 0 }}
+              transition={{
+                type: "spring",
+                stiffness: 200,
+                damping: 15,
+                delay: 0.8,
+              }}
+              className="mt-6 relative w-full h-[250px] xl:h-[300px] hidden md:block"
+            >
+              <Image 
+                src="/work-cat.png"
+                alt="Cat working"
+                fill
+                className="object-contain object-left"
+              />
+            </motion.div>
           </div>
 
           {/* slider */}
